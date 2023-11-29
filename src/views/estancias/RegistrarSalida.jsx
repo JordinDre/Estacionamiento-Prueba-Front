b3b01bc99cad8612655492b84fa6d9ee549cb127
Jordin de León <jordindre77@outlook.com>
2023-11-29 15:39:22 -0600

    salidas

--- src/views/estancias/RegistrarSalida.jsx
+++ src/views/estancias/RegistrarSalida.jsx
@@ -12,56 +12,26 @@ import Swal from 'sweetalert2';
 export default function RegistrarSalida() {
     const [cargando, setCargando] = useState(false);
     const [errores, setErrores] = useState([]);
-    const [tiposVehiculos, setTiposVehiculos] = useState([]);
-    const [sugerenciasPlaca, setSugerenciasPlaca] = useState([]);
-    const [mostrarSugerencias, setMostrarSugerencias] = useState(false);
-    const [vehiculoSeleccionado, setVehiculoSeleccionado] = useState(null);
+    const [vehiculos, setVehiculos] = useState([]);
 
     useEffect(() => {
         Promise.all([
-            axios.get('catalogo/tipos_vehiculos'),
+            axios.get('catalogo/vehiculos_adentro'),
         ])
             .then(function (responses) {
-                setTiposVehiculos(responses[0].data);
+                setVehiculos(responses[0].data);
                 setCargando(false);
             });
     }, []);
-
-    const buscarPlaca = async (texto) => {
-        if (!texto) {
-            setSugerenciasPlaca([]);
-            setVehiculoSeleccionado(null);
-            return;
-        }
-        try {
-            const response = await axios.get(`/vehiculos/buscar/${texto}`);
-            setSugerenciasPlaca(response.data);
-        } catch (error) {
-            console.error("Error al buscar placas:", error);
-        }
-    };
-
-    const seleccionarPlaca = async (placa, setFieldValue) => {
-        try {
-            const response = await axios.get(`/vehiculos/${placa}`);
-            setVehiculoSeleccionado(response.data);
-            setFieldValue("placa", placa);
-            setMostrarSugerencias(false);
-        } catch (error) {
-            console.error("Error al seleccionar la placa:", error);
-            setVehiculoSeleccionado(null);
-        }
-    };
-
 
     const handleSubmit = async (values, { resetForm }) => {
         setCargando(true);
         setErrores([]);
 
         try {
-            const response = await axios.post(`estancias`, values);
+            const response = await axios.put(`estancias/${values.vehiculo.id}`);
             Swal.fire({
-                title: 'Entrada Registrada',
+                title: 'Salida Registrada',
                 html: response.data.message,
                 icon: 'success',
                 confirmButtonText: 'Aceptar'
@@ -76,13 +46,10 @@ export default function RegistrarSalida() {
     };
 
     const validate = Yup.object().shape({
-        /*         placa: Yup.string().required("La placa es obligatoria"),
-                tipo_vehiculo: Yup.string().required("El tipo de vehículo es obligatorio"), */
     });
 
     const initial = {
-        placa: '',
-        tipo_vehiculo: ''
+        placa: [],
     };
     return (
         <>
@@ -98,45 +65,12 @@ export default function RegistrarSalida() {
             >
                 {({ setFieldValue, handleChange }) => (
                     <Form>
-                        <div className="relative my-4">
-                            <label htmlFor='placa' className="cursor-pointer text-md font-semibold">
-                                Placa
-                            </label>
-                            <Field
-                                name="placa"
-                                as="input"
-                                className="bg-white border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                autoComplete="off"
-                                placeholder="Ingrese la placa"
-                                onChange={(e) => {
-                                    handleChange(e);
-                                    buscarPlaca(e.target.value);
-                                    setMostrarSugerencias(true);
-                                }}
-                                onFocus={() => setMostrarSugerencias(true)}
-                            />
-                            {mostrarSugerencias && sugerenciasPlaca.length > 0 && (
-                                <div className="absolute z-10 w-full border border-gray-300 bg-white rounded-md shadow-lg">
-                                    {sugerenciasPlaca.map((sugerencia, index) => (
-                                        <div
-                                            key={index}
-                                            className="p-2 hover:bg-gray-100 cursor-pointer"
-                                            onClick={() => seleccionarPlaca(sugerencia.placa, setFieldValue)}
-                                        >
-                                            {sugerencia.placa}
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                        {(!vehiculoSeleccionado || !vehiculoSeleccionado.tipo_vehiculo_id) && (
-                            <CustomSelect
-                                label="Tipo de Vehiculo*"
-                                name="tipo_vehiculo"
-                                options={tiposVehiculos}
-                                customOptions={['tipo_vehiculo']}
-                            />
-                        )}
+                        <CustomSelect
+                            label="Placa*"
+                            name="vehiculo"
+                            options={vehiculos}
+                            customOptions={['vehiculo.placa']}
+                        />
                         {cargando ? <Spinner /> : <Button type='submit' className='mt-4' label='Guardar' color='blue' />}
                     </Form>
                 )}
